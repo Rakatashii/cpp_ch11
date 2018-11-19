1e3d827e42f6d04f05ce6b3a0e9d75e25bd51e0f
Christian Meyer <cmeyerwps@gmail.com>
2018-11-19 15:27:12 -0500

    Selection_Sort

---
+++ selection_sort/Build/Intermediates.noindex/selection_sort.build/Debug/selection_sort.build/Objects-normal/x86_64/util.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/christianmeyer/Cpp/ch11/selection_sort/selection_sort/util.cpp \
+  /Users/christianmeyer/Cpp/ch11/selection_sort/selection_sort/util.hpp
--- selection_sort/selection_sort/main.cpp
+++ selection_sort/selection_sort/main.cpp
@@ -1,15 +1,38 @@
-//
-//  main.cpp
-//  selection_sort
-//
-//  Created by Christian Meyer on 11/19/18.
-//  Copyright © 2018 Christian Meyer. All rights reserved.
-//
-
 #include <iostream>
+#include <vector>
+#include "util.hpp"
+
+using namespace std;
+
+int min_position(vector<int>&a , int from, int to);
+void selection_sort(vector<int>& a);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    UTIL::rand_seed();
+    vector<int> v(20);
+    for (int i = 0; i < v.size(); i++){
+        v[i] = UTIL::rand_int(1, 100);
+    }
+    UTIL::print_vector(v, "(before sort) v");
+    cout << "\n";
+    selection_sort(v);
+    UTIL::print_vector(v, "(after sort) v");
+    cout << "\n";
     return 0;
+}
+
+int min_position(vector<int>&a , int from, int to){
+    int min_pos = from;
+    for (int i = from + 1; i <= to; i++)
+        if (a[i] < a[min_pos]) min_pos = i;
+    return min_pos;
+};
+void selection_sort(vector<int>& a){
+    int next; // This is the next position to be set to the minimum.
+    for (next = 0; next < a.size() - 1; next++){
+        int min_pos = min_position(a, next, a.size() - 1);
+        if (min_pos != next){
+            UTIL::swap(a[min_pos], a[next]);
+        }
+    }
 }
